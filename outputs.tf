a522346bd129765ca8873e43adb39a6e10bf6009
chut-th <chutchawin.th@outlook.com>
2024-12-08 17:42:08 +0700

    Remove double quote of output value

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "subnet_id" {
   description = "The subnet ID."
-  value       = "azurerm_subnet.subnet.id"
+  value       = azurerm_subnet.subnet.id
 }
 
 output "subnet_name" {
   description = "The name of the subnet."
-  value       = "azurerm_subnet.subnet.name"
+  value       = azurerm_subnet.subnet.name
 }
